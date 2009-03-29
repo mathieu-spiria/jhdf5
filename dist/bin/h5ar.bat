7e1e00d1fc3e7d9ea83e7e70d3e7243156854591
brinn <brinn>
2009-03-29 15:15:41 +0000

    add: example scripts for the JHDF5 archiver
    SVN: 10449

---
+++ dist/bin/h5ar.bat
@@ -0,0 +1,4 @@
+@echo off
+rem Example script, adapt to your system
+
+java -jar jhdf5-batteries_included_lin_win_mac_sun.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ dist/bin/h5ar.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+# Example script, adapt to your system
+
+java -jar jhdf5-batteries_included_lin_win_mac_sun.jar "$@"
