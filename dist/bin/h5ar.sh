e234a2f549840b6fb4e97ecb59d7238047721b27
brinn <brinn>
2009-04-04 17:20:12 +0000

    fix: detection of JHDFDIR for relative symbolic links
    SVN: 10580

--- dist/bin/h5ar.sh
+++ dist/bin/h5ar.sh
@@ -3,12 +3,16 @@
 # This script requires the the readlink binary. If your system lacks this binary, $JHDFDIR needs to be hard-coded
 
 SCRIPT="$0"
+BINDIR="${SCRIPT%/*}"
 LINK="`readlink $0`"
-while [ -n "$LINK" ]; do
-  SCRIPT="${LINK}"
+while [ -n "${LINK}" ]; do
+  if [ "${LINK#/}" = "${LINK}" ]; then
+    SCRIPT="${BINDIR}/${LINK}"
+  else
+    SCRIPT="${LINK}"
+  fi
   LINK="`readlink ${SCRIPT}`"
 done
 BINDIR="${SCRIPT%/*}"
 JHDFDIR="${BINDIR%/*}"
-
 java -Dnative.libpath="${JHDFDIR}/lib/native" -jar "${JHDFDIR}/lib/jhdf5-tools.jar" "$@"
