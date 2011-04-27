5fe3aeb3149609992baac2d8bd49743634fb12ca
brinn <brinn>
2011-04-27 18:57:35 +0000

    add: flag -m32 to 32bit built
    SVN: 21065

--- source/c/compile_linux_i386.sh
+++ source/c/compile_linux_i386.sh
@@ -2,4 +2,4 @@
 
 source version.sh
 
-gcc -shared -O3 *.c -I/opt/hdf5-${VERSION}-i386/include -I/usr/java/jdk1.5.0_16/include -I/usr/java/jdk1.5.0_16/include/linux /opt/hdf5-${VERSION}-i386/lib/libhdf5.a -o libjhdf5.so -lz
+gcc -m32 -shared -O3 *.c -I/opt/hdf5-${VERSION}-i386/include -I/usr/java/jdk1.5.0_16/include -I/usr/java/jdk1.5.0_16/include/linux /opt/hdf5-${VERSION}-i386/lib/libhdf5.a -o libjhdf5.so -lz
