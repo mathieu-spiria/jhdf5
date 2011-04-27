57332bdfbc4fea53fcc6f24895b5c21af0fd8695
brinn <brinn>
2011-04-27 18:57:21 +0000

    change: minimal OS version for 64bit version is now Mac OS X 10.5
    SVN: 21064

--- source/c/compile_macosx_x86_64.sh
+++ source/c/compile_macosx_x86_64.sh
@@ -2,4 +2,4 @@
 
 source version.sh
 
-gcc -m64 -mmacosx-version-min=10.4 -dynamiclib -O3 *.c -I/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers -I/opt/hdf5-${VERSION}-x86_64/include /opt/hdf5-${VERSION}-x86_64/lib/libhdf5.a -lz -o libjhdf5.jnilib
+gcc -m64 -mmacosx-version-min=10.5 -dynamiclib -O3 *.c -I/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers -I/opt/hdf5-${VERSION}-x86_64/include /opt/hdf5-${VERSION}-x86_64/lib/libhdf5.a -lz -o libjhdf5.jnilib
