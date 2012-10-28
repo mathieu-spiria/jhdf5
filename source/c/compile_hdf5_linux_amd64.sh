650846dad4e978a3fe7b862709465a7d58e27507
brinn <brinn>
2012-10-28 15:39:56 +0000

    Apply linker optimization also to gcc 4.7 and 4.8.
    SVN: 27384

--- source/c/compile_hdf5_linux_amd64.sh
+++ source/c/compile_hdf5_linux_amd64.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-CFLAGS='-fPIC -m64' ./compile_hdf5_gcc.sh amd64 "gcc-4.6-optimizations-config.patch"
+CFLAGS='-fPIC -m64' ./compile_hdf5_gcc.sh amd64 "gcc-4.678-optimizations-config.patch"
--- source/c/compile_hdf5_linux_i386.sh
+++ source/c/compile_hdf5_linux_i386.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-CFLAGS='-m32' ./compile_hdf5_gcc.sh i386 "gcc-4.6-optimizations-config.patch"
+CFLAGS='-m32' ./compile_hdf5_gcc.sh i386 "gcc-4.678-optimizations-config.patch"
