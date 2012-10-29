b98bf6dd590ddd6c6ce5d23c661d175fff85f900
brinn <brinn>
2012-10-29 14:20:39 +0000

    Add link-time optimization to libjhdf5.so for Linux.
    SVN: 27399

--- source/c/compile_linux_amd64.sh
+++ source/c/compile_linux_amd64.sh
@@ -2,4 +2,4 @@
 
 source version.sh
 
-gcc -shared -O3 -fPIC jhdf5/*.c hdf-java/*.c -I/opt/hdf5-${VERSION}-amd64/include -I/usr/java/jdk1.5.0_16/include -I/usr/java/jdk1.5.0_16/include/linux /opt/hdf5-${VERSION}-amd64/lib/libhdf5.a -o libjhdf5.so -lz
+gcc -shared -O3 -floop-interchange -floop-strip-mine -floop-block -fgraphite-identity -fuse-linker-plugin -flto -mtune=corei7 -fPIC jhdf5/*.c hdf-java/*.c -I/opt/hdf5-${VERSION}-amd64/include -I/usr/java/jdk1.6.0_37/include -I/usr/java/jdk1.6.0_37/include/linux /opt/hdf5-${VERSION}-amd64/lib/libhdf5.a -o libjhdf5.so -lz
