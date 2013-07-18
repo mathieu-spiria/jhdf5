54d6fee675094f06dc94a4a9add5bf7c42612793
brinn <brinn>
2013-07-18 16:31:56 +0000

    Make antrun.sh independent of from which working directory it is called.
    SVN: 29559

--- build/antrun.sh
+++ build/antrun.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-ME="$0"
-MYDIR=${ME%/*}
-cd $MYDIR
-ant -lib ../../build_resources/lib/ecj.jar "$@"
+SCRIPT="$0"
+if [ "${SCRIPT}" = "${SCRIPT#/}" ]; then
+  SCRIPT="`pwd`/${SCRIPT#./}"
+fi
+BINDIR="${SCRIPT%/*}"
+BASEDIR=${BINDIR%/*}
+cd $BASEDIR
+ant -lib ../../build_resources/lib/ecj.jar -f build/build.xml "$@"
