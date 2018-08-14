b82010151ea130ad0d42ea7fae96fe4ae8ea154d
Bernd Rinn <brinn@ethz.ch>
2018-08-14 22:12:23 +0200

    Update Windows x64 build.

---
+++ source/c/prepare_winbuild.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+source version.sh
+
+rm -fR build
+mkdir build
+
+cd build
+BUILD_ROOT=`pwd`
+
+unzip ../CMake-hdf5-$VERSION.zip
+
+if [ -n "$POSTFIX" ]; then
+  mv CMake-hdf5-$VERSION CMake-hdf5-$VERSION-$POSTFIX
+fi
+
+rm -fR CMake-hdf5-$VERSION/hdf5-$VERSION/java/src/jni
+cp -af ../jni CMake-hdf5-$VERSION/hdf5-$VERSION/java/src/
+cp -af ../*.c CMake-hdf5-$VERSION/hdf5-$VERSION/java/src/jni/
+
+patch -p0 < ../cmake_set_hdf5_options.diff
+patch -p0 < ../cmake_add_sources.diff
