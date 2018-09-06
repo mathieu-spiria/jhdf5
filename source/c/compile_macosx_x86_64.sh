aeaec2c61dc75a2eb06cb737b6048094faa35fed
Bernd Rinn <brinn@ethz.ch>
2018-09-06 23:59:03 +0200

    Remove double /.

--- source/c/compile_macosx_x86_64.sh
+++ source/c/compile_macosx_x86_64.sh
@@ -15,7 +15,7 @@ cp hdf5-$VERSION/src/H5win32defs.h jni/
 cp hdf5-$VERSION/src/H5private.h jni/
 
 echo "JHDF5 building..."
-gcc -m64 -mmacosx-version-min=10.11 -dynamiclib -O3 jni/*.c -Ihdf5-${VERSION}-x86_64/include -I/Applications//Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers hdf5-${VERSION}-x86_64/lib/libhdf5.a -o libjhdf5.jnilib -lz &> jhdf5_build.log
+gcc -m64 -mmacosx-version-min=10.11 -dynamiclib -O3 jni/*.c -Ihdf5-${VERSION}-x86_64/include -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers hdf5-${VERSION}-x86_64/lib/libhdf5.a -o libjhdf5.jnilib -lz &> jhdf5_build.log
 
 if [ -f libjhdf5.jnilib ]; then
   cp -pf libjhdf5.jnilib "../../../libs/native/jhdf5/x86_64-Mac OS X"
